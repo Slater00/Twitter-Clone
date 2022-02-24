8d9eabe599518f613d9414b3ef146a3344aaea32
Slater1111 <slatergray1111@gmail.com>
2022-02-24 15:45:37 -0500

    update

---
+++ posts/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import redirect, render
+from django.http import HttpResponseRedirect, HttpResponse
+from.models import Post
+from .forms import PostForm
+
+def index(request):
+     if request.method == 'POST':
+          form = PostForm(request.POST, request.FILES)
+          if form.is_valid():
+               form.save()
+               return HttpResponseRedirect('/')
+          else:
+               return HttpResponseRedirect(form.errors.as_json())
+
+     #Get all posts, limit = 20 
+     posts = Post.objects.all().order_by('-created_at')[:20] 
+
+
+     return render(request, 'posts.html',
+              {'posts': posts})
+
+def delete(request, post_id):
+     post = Post.objects.get(id = post_id)
+     post.delete()
+     return HttpResponseRedirect('/')
+   
+def like(request, post_id):
+     newlikecount = Post.objects.get(id = post_id)
+     newlikecount.likecount += 1
+     newlikecount.save()
+     return HttpResponseRedirect('/')
+
+def edit(request, post_id):
+     posts = Post.objects.get(id = post_id)
+     if request.method == 'POST':
+          form = PostForm(request.POST, request.FILES, instance=posts)
+          if form.is_valid():
+               form.save()
+               return HttpResponseRedirect('/')
+          else:
+               return HttpResponseRedirect("not valid") 
+     return render(request, 'edit.html',{'posts': posts})
